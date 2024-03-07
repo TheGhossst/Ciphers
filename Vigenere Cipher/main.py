5629902633a50598c57c7b49f85c4efa227d9e34
TheGhossst <nandukrishnapkm97@gmail.com>
2024-03-07 20:29:43 +0530

    success

--- Vigenere Cipher/main.py
+++ Vigenere Cipher/main.py
@@ -10,22 +10,29 @@ def generateKey(key,ptLen):
         modifiedKey.append(modifiedKey[i % keyLen])
     key = "".join(modifiedKey)
     return key
+
+def encrypt(plainText,key):
+    plainTextNumbers = []
+    for c in plainText:
+        #plainTextNumbers.append(((ord(c) % 26)+ 7) % 26)
+        plainTextNumbers.append((ord(c)+ 7) % 26)
+    print(plainTextNumbers)
    
 def main():
     key = input("Enter the key value : ")
-    #plainText = "Hello World"
-
+    
     with open('input.txt', 'r') as file:
         lines = file.readlines()
         plainText = ''.join(lines)
     print(plainText)
 
-    
     ptLen = len(plainText)    
     key = generateKey(key,ptLen)
     print(f"The modified key is : {key}") 
     print(f"The length of the key is : {len(key)}")
     print(f"The length of the plain text is : {ptLen}")  
     
+    encrypt(plainText,key)
+   
 if __name__ =="__main__":
     main()
