5781c15f300b08286f932ffac278494e7c67676a
TheGhossst <nandukrishnapkm97@gmail.com>
2024-03-07 20:14:07 +0530

    working?

--- Vigenere Cipher/main.py
+++ Vigenere Cipher/main.py
@@ -13,10 +13,19 @@ def generateKey(key,ptLen):
    
 def main():
     key = input("Enter the key value : ")
-    plainText = "Hello World"
+    #plainText = "Hello World"
+
+    with open('input.txt', 'r') as file:
+        lines = file.readlines()
+        plainText = ''.join(lines)
+    print(plainText)
+
+    
     ptLen = len(plainText)    
     key = generateKey(key,ptLen)
     print(f"The modified key is : {key}") 
+    print(f"The length of the key is : {len(key)}")
+    print(f"The length of the plain text is : {ptLen}")  
     
 if __name__ =="__main__":
     main()
