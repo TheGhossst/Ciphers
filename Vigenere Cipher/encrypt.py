d47961dd7d606778520606ef46e16b2011b11512
TheGhossst <nandukrishnapkm97@gmail.com>
2024-03-07 20:58:24 +0530

    finale

--- Vigenere Cipher/encrypt.py
+++ Vigenere Cipher/encrypt.py
@@ -19,40 +19,34 @@ def encrypt(plainText, key):
     
     for c in plainText:
         plainTextNumbers.append((ord(c) - ord('A')) % 26)
-    print("Plaintext numbers:", plainTextNumbers)
 
     for c in key:
         keyNumbers.append((ord(c) - ord('A')) % 26)
-    print("Key numbers:", keyNumbers)
     
     for i in range(len(plainText)):
         cipherTextNumbers.append((plainTextNumbers[i] + keyNumbers[i % len(key)]) % 26)
     
     for num in cipherTextNumbers:
         cipherText.append(chr(num + ord('A')))
-    print(cipherText)
+    
     
     return ''.join(cipherText)
     
     
 def main():
+    fileName = input("Enter the name of the file (without extentions) :")
     key = input("Enter the key value : ")
     
-    with open('input.txt', 'r') as file:
+    with open(fileName + ".txt", 'r') as file:
         lines = file.readlines()
         plainText = ''.join(lines)
-    print(plainText)
-
-    #plainText = plainText.upper()
+        
     ptLen = len(plainText)    
     key = generateKey(key,ptLen)
-    #key = key.upper()
-    print(f"The modified key is : {key}") 
-    print(f"The length of the key is : {len(key)}")
-    print(f"The length of the plain text is : {ptLen}")  
-    
     cipherText = encrypt(plainText,key)
-    print(cipherText)
-   
+
+    with open(fileName + "-enc.txt", 'w') as file:
+        file.write(cipherText)
+
 if __name__ =="__main__":
     main()
